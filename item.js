3b08b3feb50616112c012451fb7eadbb58bde383
jkume <j-kume@aisic.jp>
2019-03-19 11:23:31 +0900

    Therd commit

--- item.js
+++ item.js
@@ -2,13 +2,13 @@
 //【ファイル名】
 //    item.js
 //【アプリ名】
-//    商品マスタ
+//    XXXマスタ
 //【備考】
 //
 //【タイプ】
 //    ●PC用 / ○スマートフォン用
 //-----------------------------------------------------------------------------
-//  Copyright (c) 2019 Sonic Garden Inc.
+//  Copyright (c) 2019 XXXXXXX Inc.
 //=============================================================================
 
 (function () {
@@ -23,35 +23,7 @@
     ], function (event) {
         let record = event.record;
 
-        // kintoneからのデータ取得
-        return kintone.api(kintone.api.url('/k/v1/records', true), 'GET', {
-            "app": kintone.app.getId(),
-            "query": 'order by uid desc limit 1',
-            "totalCount": true
-        }).then(
-            function(resp) {
-                console.log("商品情報取得完了");
-                console.log(resp);
-
-                if (Number(resp.records.length) === 0) {
-                    window.alert('対象データがありませんでした。');
-                } else {
-
-                    // 取得した商品データを格納
-                    let itemRecords = resp.records;
-
-                    record.uid.value = Number(itemRecords[0].uid.value) + 1;
-                    record.no.value = ('0000' + record.uid.value).slice(-4);
-                    record.lookup_key.value = record.no.value + ' ' + record.name.value;
-
-                    return event;
-                }
-            },
-            function(error) {
-                console.log(error);
-                console.log("データ取得失敗");
-                return event;
-            });
+        return event;
     }); // end kintone.events.on ['app.record.create.submit']
 
     // =============================================
@@ -63,8 +35,6 @@
     ], function (event) {
         let record = event.record;
 
-        record.lookup_key.value = record.no.value + ' ' + record.name.value;
-
         return event;
     }); // end kintone.events.on [beforeEditSaveEvents]
 
@@ -75,9 +45,6 @@
         'app.record.create.show','app.record.edit.show', 'app.record.index.edit.show'
     ], function (event) {
         let record = event.record;
-        record.uid.disabled = true;
-        record.no.disabled = true;
-        record.lookup_key.disabled = true;
         return event;
     }); // end kintone.events.on ['app.record.create.show','app.record.edit.show', 'app.record.index.edit.show']
 
